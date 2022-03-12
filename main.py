19fda62c88f1a73100d1c40a61966043a29c4e0f
tunahan.pinar <tunahan.pinar@vlmedia.com.tr>
2022-03-12 21:38:38 +0300

    change path

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from kuwahara import kuwahara
 from mean import mean
 
 if __name__ == '__main__':
-    path = './img.png'
+    path = './images/img.png'
     src_img = cv2.imread(path)
 
     res_kuwahara = kuwahara(src_img, 3)
@@ -16,9 +16,3 @@ if __name__ == '__main__':
     cv2.imwrite('output_mean.png', res_mean)
     cv2.imwrite('output_gaussian.png', res_gaussian)
 
-
-    #for i in [3, 5, 9, 15]:
-    ##    start = time.time()
-     #   res_gaussian = kuwahara(src_img, i)
-      #  print(f'completed mean with {i} in   {time.time() - start} sec')
-     #   cv2.imwrite(f'output_kuwahara_{i}.png', res_gaussian)
